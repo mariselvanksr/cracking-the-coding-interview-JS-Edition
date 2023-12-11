773f878e78cb0cda1e6aa4e5e0d038a303ab3c8c
Mariselvan <mariselvanbe2012@gmail.com>
2023-12-11 05:43:36 +0530

    Stack implementation with get min at O(1) time

---
+++ Cracking the Coding Interview/Chapter 3/stack-min.js
@@ -0,0 +1,79 @@
+/**
+ * How would you design a stack which, in addition to push and pop, has a function min which returns the minimum element? Push, Pop and Min should operate at O(1) time.
+ */
+
+/**
+ * Solution:
+ *  If we only keep one min variable when the user pops out value from the stack, we have to 
+ *  calculate the latest min. This breaks the O(1) constrain.
+ * 
+ * 1. Create a node which holds min and value.
+ * 2. On each push store the current min with value in the stack as an node object.
+ * 3. When user pops gets the last node from the stack return value.
+ * 4. When peek and getMin called, access the last node and gets value or min property accordingly.
+ */
+
+class Node {
+    constructor(value, min) {
+        this.value = value;
+        this.min = min;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.stack = [];
+    }
+
+    push(value) {
+        let currentMin = value;
+
+        if(this.peek()) {
+            let peek = this.peek();
+
+            currentMin = currentMin > peek.min ? peek.min : currentMin;
+        }
+
+        let newNode = new Node(value, currentMin)
+
+        this.stack.push(newNode);
+    }
+
+    peek() {
+        if(!this.stack.length) return false;
+
+        return this.stack[this.stack.length - 1];
+    }
+
+    getMin() {
+        if(!this.stack.length) return false;
+
+        return this.stack[this.stack.length - 1].min;
+    }
+
+    pop() {
+        if(!this.stack.length) return false;
+
+        let pop = this.stack.pop();
+
+        return pop.value;
+    }
+}
+
+let stack = new Stack();
+
+stack.push(10)
+stack.push(5)
+stack.push(6)
+stack.push(8)
+stack.push(3)
+stack.push(2)
+stack.push(9)
+
+console.log(stack.getMin())
+stack.pop()
+stack.pop()
+console.log(stack.getMin())
+stack.pop()
+console.log(stack.getMin())
+stack.pop()
